9cb4743b3f1c0c4e626b87ec88f5b87289e57fa4
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-08 10:11:00 +0000

    doc improvements

--- src/main/java/org/jenkinsci/plugins/mastertomasterapi/InterMasterService.java
+++ src/main/java/org/jenkinsci/plugins/mastertomasterapi/InterMasterService.java
@@ -2,16 +2,26 @@ package org.jenkinsci.plugins.mastertomasterapi;
 
 import hudson.ExtensionList;
 import hudson.ExtensionPoint;
+import hudson.remoting.Channel;
 import jenkins.model.Jenkins;
 
 import javax.annotation.CheckForNull;
 
 /**
+ * An extension point for exposing inter-master service over {@link Master#getService(Class)}.
+ *
+ * <p>
+ * An object returned from this class will be proxied (possibly by {@link Channel} or something else)
+ * to another master.
+ *
  * @author Kohsuke Kawaguchi
  */
 public abstract class InterMasterService implements ExtensionPoint {
     /**
      * Creates a instance that receives inter-master calls.
+     *
+     * @param remote
+     *      {@link Master} that the returned instance will serve.
      */
     @CheckForNull
     public abstract <T> T getInstance(Class<T> interfaceType, Master remote);
